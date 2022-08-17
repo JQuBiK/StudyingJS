d55983b02e88b0021e18af8535507c3fd6369232
Igor <freezof1x_1103@mail.ru>
2022-08-17 12:34:12 +0300

    new commit

---
+++ js/script.js
@@ -0,0 +1,133 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+// obj.a = 2;
+// console.log(obj);
+
+// // snake_case
+// // UPPER_SNAKE_CASE
+// // cammleCase
+// // Kebab-case
+// // PascalCase
+
+// const persone = "Alex";
+// const bool = true;
+
+// const obj = {
+//     name: "Igor",
+//     age: 20,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let arr = [
+//     'plum.png',
+//     'orangr.jpg',
+//     'apple.bmp',
+//     6,
+//     {
+//         age: 80
+//     },
+//     'Igor'
+// ];
+
+// console.log(arr[5]);
+
+
+// const arr = [1, 2, 3];
+
+// arr[10] = '3456';
+
+// console.log(arr);
+
+// const obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// };
+
+// const obj = {
+//     Anna: 500,
+//     Alice: 800
+// };
+
+// console.log(obj.Anna);
+
+
+// const d = 'd';
+// const arrObj = {
+//     a: "a",
+//     b: "b",
+//     c: "c",
+//     abc: {
+//         def: {
+
+//         }
+//     }
+// };
+
+// arrObj.d = '1234';
+// arrObj[d] = '1234';
+// console.log(arrObj['d']);
+// console.log(arrObj.d);
+
+
+// let storeName = 'MovieStore';
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Alex', 'Anna', 'Alice'],
+//     products: {
+//        ' cartoon': 400,
+//         'movie': 900
+//     },
+//     open: true
+// }
+
+// alert('Hello World');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+// if (result == true) {
+//     alert('Hello!');
+// } else {
+//     alert('Goodbye!');
+// }
+
+// const answer = +prompt('Do you have 18?', 'Sure');
+// console.log(answer);
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('What is youre name?');
+// answers[1] = prompt('What is youre surname?');
+// answers[2] = prompt('How old are you?');
+
+// console.log(typeof(answers));
+
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com.${category}/6`);
+
+// const user = prompt('What is youre name?');
+// console.log(user);
+// alert(`Hello, ${user}!`);
+
+
+let incr = 10, 
+    decr = 10;
+
+console.log(decr, incr);
+
+console.log(5%2);
